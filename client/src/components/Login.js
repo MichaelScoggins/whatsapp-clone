eccfd3e7c41a45f9d767a53844d54252a55fdf24
Michael Scoggins <rhythmghosts@gmail.com>
2021-02-28 19:47:29 -0600

    added some state. learned about useRef()

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,28 @@
 import React, { useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 
-export default function Login() {
+export default function Login({ onIdSubmit }) {
   const idRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onIdSubmit(idRef.current.value);
+  }
+
   return (
     <Container
       className="align-items-center d-flex"
       style={{ height: "100vh" }}
     >
-      <Form className="w-100">
+      <Form onSubmit={handleSubmit} className="w-100">
         <Form.Group>
           <Form.Label>Enter Your Id</Form.Label>
           <Form.Control type="text" ref={idRef} required></Form.Control>
         </Form.Group>
-        <Button type="submit">Login</Button>
+        <Button type="submit" className="mr-2">
+          Login
+        </Button>
+        <Button variant="secondary">Create A New Id</Button>
       </Form>
     </Container>
   );
