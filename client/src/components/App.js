dfef6ff9e02416a6b82340ceccdc1d5b73110781
Michael Scoggins <rhythmghosts@gmail.com>
2021-03-02 16:47:45 -0600

    coding sidebar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Login from "./Login";
+import Dashboard from "./Dashboard";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
 
-  return (
-    <>
-      {id}
-      <Login onIdSubmit={setId} />
-    </>
-  );
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
 }
 
 export default App;
