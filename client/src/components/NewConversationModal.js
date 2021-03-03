3bffe1e29106a8b2517e201bf3773228b8a4b311
Michael Scoggins <rhythmghosts@gmail.com>
2021-03-02 23:39:42 -0600

    finished modals. working on adding ContactsContext

--- client/src/components/NewConversationModal.js
+++ client/src/components/NewConversationModal.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Modal } from "react-bootstrap";
 
-export default function NewConversationModal() {
-  return <div>NewConversationModal</div>;
+export default function NewConversationModal({ closeModal }) {
+  return (
+    <>
+      <Modal.Header closeButton>Create Contact</Modal.Header>
+    </>
+  );
 }
